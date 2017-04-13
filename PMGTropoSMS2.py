b1e45d472eab4dd4b40623a7911f5fade3d63105
waterrun <emailpmg@btopenworld.com>
2017-04-13 15:46:06 +0100

    Update PMGTropoSMS2.py

--- PMGTropoSMS2.py
+++ PMGTropoSMS2.py
@@ -4,7 +4,7 @@ url = "https://api.tropo.eu/1.0/sessions"
 
 querystring = {"action":"create","token":"547a797266697277416756594c446a466a427a48484e6c434f74434f7a596e50566f44584879757a75616461"}
 
-payload = "myString=Adding%20lots%20of%20text%20in%20test%207%20ProntoPro&myNumber=%2B447900550902"
+payload = "myString=FromGitHub%20Adding%20lots%20of%20text%20in%20test%207%20ProntoPro&myNumber=%2B447900550902"
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache",
