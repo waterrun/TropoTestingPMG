fa5069bf032dec270edac838bdccd2e42ea3b9a5
waterrun <emailpmg@btopenworld.com>
2017-04-13 15:23:34 +0100

    Create PMGTropoTokenSMS.py

---
+++ samples/PMGTropoTokenSMS.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://api.tropo.eu/1.0/sessions"
+
+querystring = {"action":"create","token":"547a797266697277416756594c446a466a427a48484e6c434f74434f7a596e50566f44584879757a75616461"}
+
+payload = "myString=Adding%20lots%20of%20text%20in%20test%207%20ProntoPro&myNumber=%2B447900550902"
+headers = {
+    'content-type': "application/x-www-form-urlencoded",
+    'cache-control': "no-cache",
+    'postman-token': "c22ed1f2-0b27-e2a0-a0f7-9e50b96a8809"
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
+print(response.text)
